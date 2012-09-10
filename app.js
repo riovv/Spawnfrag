da8133d601f346c0b3acbd29a20830fa6c9e78ab
Patric Nordmark <patric.nordmark@gmail.com>
2012-09-10 18:38:07 +0200

    Moved header template to an include file named header.jade.

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.configure('development', function () {
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Spawnfrag server listening on port " + app.get('port'));
 });
